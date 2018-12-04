d5ef96331bbcc883251712fa49d48a0511171d5b
dadi <675834325@qq.com>
2018-12-04 17:16:07 +0800

    delete

--- src/config/constants.js
+++
@@ -1,9 +0,0 @@
-// modal type
-export const ENTITY_DETAIL = 'entity_detail';
-export const ENTITY_LIST = 'entity_list';
-export const ENTITY_RECEIVE = 'entity_receive';
-export const ENTITY_CONFIRM = 'entity_confirm';
-export const ENTITY_COMPANY = 'entity_company';
-export const ENTITY_STATUS = 'entity_status';
-export const ENTITY_ACCOUNT = 'entity_account';
-export const ENTITY_RECORD = 'entity_record';
